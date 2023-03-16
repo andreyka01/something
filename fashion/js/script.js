b16d5f4f07f504b1d3019df85f774c5782a18ef5
Andrew Kurash <88962283+andreyka01@users.noreply.github.com>
2023-03-16 11:04:05 +0200

    Delete fashion directory

--- fashion/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('click', documnetClick);
-
-function documnetClick(e) {
-   const targetItem = e.target;
-
-   if(targetItem.closest('.icon-menu')) {
-      document.documentElement.classList.toggle('menu-open');
-   }
-}
