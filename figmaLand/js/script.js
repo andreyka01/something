fa30fe6d591d6a5f63bfc9065bbbb2345612eeeb
Andrew Kurash <88962283+andreyka01@users.noreply.github.com>
2023-03-31 16:59:04 +0300

    Delete figmaLand directory

--- figmaLand/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', documentClick);
-
-function documentClick(e) {
-   const targetItem = e.target;
-   if(targetItem.closest('.icon-menu')) {
-      document.documentElement.classList.toggle('menu-open');
-   }
-}
